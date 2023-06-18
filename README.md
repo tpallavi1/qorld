8525881558c582a08c98d704e205df6617688c1b
tpallavi1 <136848887+tpallavi1@users.noreply.github.com>
2023-06-18 13:20:52 -0400

    a fluttter project to show world time

---
+++ README.md
@@ -0,0 +1,2 @@
+# world_time
+ Shows what is the time different parts of world
---
+++ lib/pages/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/home.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State Home> createState() =>  HomeState();
+}
+
+class  HomeState extends State Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
